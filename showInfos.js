06ae9f8f5b4083fccd5fa3c3f7858f61db69c18c
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2019-12-05 21:27:12 +0100

    Correction de l'affichage de annonces en haut à gauche de chaque pages, correction du bug d'affichage sur le profil pour le nombre de places restantes, ajout de la smart_recherche

--- showInfos.js
+++ showInfos.js
@@ -17,20 +17,36 @@ function placeLeft(reserved, available){
 
 function annoncesHtml(queryRes){
     let toRet = ""
-    console.log(queryRes);
+    //console.log(queryRes);
     for(var i=0;i<queryRes.length;i++){
         toRet += "<tr onClick='document.location=\"/fifthpage?id="+ queryRes[i]._id +"\"'><td>" + queryRes[i].user + "</td><td>" + queryRes[i].ddepart + "</td><td>" + queryRes[i].ldepart + "</td><td>" + queryRes[i].larrivee + "</td><td>" + placeLeft(queryRes[i].reserved.length, queryRes[i].places) + "</td></tr>"
     }
     return toRet;
 }
 
+function formatQuery(query, searchParam){ //carefull inputs need to be in a list format and they have to have the same length
+    //console.log(query);
+    if(query.length != searchParam.length){
+        console.log("formatQuery input format not respected");
+        return {};
+    }
+    let format = {};
+    for(var i=0;i<query.length;i++){
+        if(query[i] != ""){
+            format[searchParam[i]] = query[i];
+        }
+    }
+    //console.log(format);
+    return format;
+}
+
 function annoncesprofilHtml(req, queryRes){
 	sesUsername = req.session.username;
     let toRet = ""
-    console.log(queryRes);
+    //console.log(queryRes);
     for(var i=0;i<queryRes.length;i++){
 		if (sesUsername == queryRes[i].user) {
-			toRet += "<tr onClick='document.location=\"/fifthpage?id="+ queryRes[i]._id +"\"'><td>" + queryRes[i].ddepart + "</td><td>" + queryRes[i].ldepart + "</td><td>" + queryRes[i].larrivee + "</td><td>" + placeLeft(queryRes[i].reserved, queryRes[i].places) + "</td></tr>"
+			toRet += "<tr onClick='document.location=\"/fifthpage?id="+ queryRes[i]._id +"\"'><td>" + queryRes[i].ddepart + "</td><td>" + queryRes[i].ldepart + "</td><td>" + queryRes[i].larrivee + "</td><td>" + placeLeft(queryRes[i].reserved.length, queryRes[i].places) + "</td></tr>"
 		}
     }
     return toRet;
@@ -52,10 +68,14 @@ exports.firstPage = function(req, res){
         }
         var query = {};
         if(req.query.search != null){
-            query.description = req.query.search;
+            query = formatQuery(req.query.search,['ldepart', 'larrivee']);
         }
         dbo.collection("annonces").find(query).toArray(function(err, result){
             queryRes = result;
+            if(result.length == 0){
+                var noResults = "<tr><td colspan='5' class='noResult'>Pas d'annonce trouvée</td></tr>";
+                res.render("Page1.html",{username:user, annonces: noResults, Date:getDate(), Disco : connect});
+            }
             res.render("Page1.html",{username:user, annonces: annoncesHtml(queryRes), Date:getDate(), Disco : connect});
         })
         
